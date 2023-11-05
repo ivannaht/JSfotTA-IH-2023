053c5c10192c2373340c132be032aa90ba670d1f
ivannaht <ivhontaryk@gmail.com>
2023-11-05 22:49:14 +0200

    Added task 4

--- app.js
+++ app.js
@@ -13,4 +13,20 @@ quality = quantity;
 option2 = `quality: ${quality} and quantity: ${quantity}`;
 document.write("<h1>option2</h1>");
 document.write(option2);
+
+// task4
+let film = {
+    name: "Finding Nemo",
+    releaseYear: 2003,
+    isInteresting: true,
+    actors: undefined,
+    budjet: null
+}
+
+console.log(typeof film.name);
+console.log(typeof film.releaseYear);
+console.log(typeof film.isInteresting);
+console.log(typeof film.actors);
+console.log(typeof film.budjet);
+
                
