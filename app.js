700dd8f44d186064720e9158027c63a9148597b5
ivannaht <ivhontaryk@gmail.com>
2023-11-05 22:30:59 +0200

    Fixed typos task 3

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ let quality;
 let quantity;
 quality = "high";
 quantity = 5;
-option1 = `quality: ${quality} and quantity: ${quantity} \t`;
+option1 = `quality: ${quality} and quantity: ${quantity}`;
 document.write("<h1>option1</h1>");
 document.write(option1);
 quality = quantity;
-option2 = `quality: ${quality} and quantity: ${quantity} \n`;
+option2 = `quality: ${quality} and quantity: ${quantity}`;
 document.write("<h1>option2</h1>");
 document.write(option2);
                
