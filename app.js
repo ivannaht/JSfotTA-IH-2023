4d2da87ae920de30d67c4539a651b95edca4c8a7
ivannaht <ivhontaryk@gmail.com>
2023-11-05 22:29:00 +0200

    Added task 3

---
+++ app.js
@@ -0,0 +1,16 @@
+// task 2
+console.log("Hontaryk");
+
+// task 3
+let quality;
+let quantity;
+quality = "high";
+quantity = 5;
+option1 = `quality: ${quality} and quantity: ${quantity} \t`;
+document.write("<h1>option1</h1>");
+document.write(option1);
+quality = quantity;
+option2 = `quality: ${quality} and quantity: ${quantity} \n`;
+document.write("<h1>option2</h1>");
+document.write(option2);
+               
