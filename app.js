489a858a94d6325943e057df89bbc146a5faa88f
ivannaht <ivhontaryk@gmail.com>
2023-11-12 17:12:45 +0200

    Added task 2.2

--- app.js
+++ app.js
@@ -109,3 +109,18 @@ console.log(typeof res3);
 let res4 = parseInt(x === y);
 console.log(res4);
 console.log(typeof res4);
+
+// task 2.2
+let inputNumber;
+
+inputNumber = prompt("Please enter a number");
+
+if (isNaN(inputNumber)) {
+    console.log(`You entered ${inputNumber}. It is not a number`);
+} else if (inputNumber % 2 == 0 && inputNumber > 0) {
+    console.log(`You entered ${inputNumber}. This number is even and positive`);
+} else if (inputNumber % 7 == 0) {
+    console.log(`You entered ${inputNumber}. This number is a multiple of 7`);
+} else {
+    console.log(`You entered ${inputNumber}. This number is incorrect`);
+}
