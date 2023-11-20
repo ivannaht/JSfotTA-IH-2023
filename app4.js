7d611efceb5b974c0e55d8aeac27360d2c6164a7
ihont <ihont@softserveinc.com>
2023-11-20 14:57:22 +0200

    Added task  4.3

--- app4.js
+++ app4.js
@@ -9,7 +9,7 @@ console.log(`width = ${width} , height = ${height}`);
 function calcRectangleArea(width, height) {
     let rectangleArea = width * height;
     if (isNaN(width) || isNaN(height)) {
-        throw new Error ("Non-numeric values were entered");      
+        throw new Error("Non-numeric values were entered");      
     }
     return rectangleArea;
 }
@@ -27,11 +27,11 @@ function checkAge() {
     let userAge = prompt("Please enter your age");
     console.log(`age = ${userAge}`);
     if (userAge === "" )  {
-        throw new Error ("The field is empty! Please enter your age");
+        throw new Error("The field is empty! Please enter your age");
     } else if (isNaN(userAge)) {
-        throw new Error ("Non-numeric value was entered! Please enter your age as a number"); 
+        throw new Error("Non-numeric value was entered! Please enter your age as a number"); 
     } else if (userAge < 14) {
-        throw new Error ("You are not permitted to watch the movie due to insufficient age");      
+        throw new Error("You are not permitted to watch the movie due to insufficient age");      
     }
     alert("Your age is fine. You get access to watching the movie!");
 }
@@ -43,4 +43,36 @@ try {
     } finally {
         console.log("Function checkAge was run");
     }
+
+//task 4.3
+class MonthException extends Error {
+    constructor(message) {
+      super(message);
+      this.name = "MonthException";
+    }
+  }
+
+const month = +prompt("Please enter the month as a number");
+console.log(month);
+ 
+
+function showMonthName(month) {
+    var months = ["January","February","March","April","May","June","July",
+    "August","September","October","November","December"];
+    if (month <= 0 || month > 12)  {
+        throw new MonthException("Incorrect month number");
+    }
+    return months[month - 1];
+}
+try {
+    showMonthName(month);
+    } catch (e) {
+        console.error(`${e.name}: ${e.message}`);
+        console.log(`${e.name} ${e.message}`);
+
+    } finally {
+        console.log("Function showMonthName was run");
+    }
+
+    console.log(showMonthName(month));
     
