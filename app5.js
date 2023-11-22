1e6db54c567a66d25a9f7c6597cb7dd9d3bd625d
ihont <ihont@softserveinc.com>
2023-11-22 23:08:49 +0200

    Added task 5.2

--- app5.js
+++ app5.js
@@ -9,7 +9,6 @@ function propsCount(currentObject) {
     return Object.keys(currentObject).length;
 }
 
-
 console.log(propsCount(fruit));
 
 fruit.name = "Lemon";
@@ -18,3 +17,19 @@ fruit.taste = "Sour";
 fruit.usage = "For tea";
 
 console.log(propsCount(fruit));
+
+// task 5.2
+let ukrainianNationalHolidays = {
+    name: "Christmas",
+    month: "December",
+    day: 25,
+    isReligious: true,
+    traditions: ["Kutia", "Koliadky", "Vertep"]
+}
+
+function showProps(obj) {
+    Object.keys(obj).forEach((key, index) => console.log(`${index}: ${key}`));
+    Object.values(obj).forEach((value, index) => console.log(`${index}: ${value}`));
+}
+
+showProps(ukrainianNationalHolidays);
