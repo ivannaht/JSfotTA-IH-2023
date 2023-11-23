a905ff54d7338813229594931ec889ef9acf17dc
ihont <ihont@softserveinc.com>
2023-11-23 17:23:57 +0200

    Added task 5.4

--- app5.js
+++ app5.js
@@ -1,9 +1,12 @@
+// Home Work 5
+console.log("Home Work 5");
+
 // task 5.1
 let fruit = {
     name: "Apple",
     color: "Red",
     taste: "Sweet"
-}
+};
 
 function propsCount(currentObject) {
     return Object.keys(currentObject).length;
@@ -25,7 +28,7 @@ let ukrainianNationalHolidays = {
     day: 25,
     isReligious: true,
     traditions: ["Kutia", "Koliadky", "Vertep"]
-}
+};
 
 function showProps(obj) {
     Object.keys(obj).forEach((key, index) => console.log(`${index}: ${key}`));
@@ -67,3 +70,96 @@ class Student extends Person {
 const stud1 = new Student("Anna", "Bojko", 2020);
 console.log(stud1.showFullName("Ivanivna"));
 console.log("Current course: " + stud1.showCourse());
+
+// task 5.4
+class Worker {
+    #experience = 1.2;
+
+    set experience(value) {
+        if (value <= 1) throw new Error("Experience must be greater than 1");
+        this.#experience = value;         
+    }
+
+    get experience() {
+        return this.#experience;
+    }
+      
+    constructor(fullName, dayRate, workingDays){
+        this.fullName = fullName;
+        this.dayRate = dayRate;
+        this.workingDays = workingDays;
+    }
+
+    showSalary() {
+        return this.dayRate * this.workingDays;
+    }
+
+    showSalaryWithExperience() {
+        return this.#experience * this.showSalary();
+    }
+
+    showExp() {
+        return this.#experience;
+    }
+
+    toString() {
+        let info = `Full Name: ${this.fullName}\n 
+        Day Rate: ${this.dayRate}\n 
+        Working Days: ${this.workingDays}\n 
+        Salary: ${this.showSalary().toFixed(2)}\n
+        New Experience: ${this.showExp()}\n 
+        Salary With Experience: ${this.showSalaryWithExperience().toFixed(2)}`;
+        return info;
+    }
+}
+
+class Workers {
+    constructor(workers) {
+        this.workers = workers;
+    }
+
+    showAllWorkers() {
+        for (const worker of this.workers) {
+            console.log(worker.toString());
+        }
+    }
+
+    sort() {
+        this.workers.sort((a, b) => 
+        a.showSalaryWithExperience() - b.showSalaryWithExperience());
+        return this.workers;
+    }
+
+    showSortedWorkers(){
+        const sortedList = this.sort();
+        console.log("Sorted salary:");
+        sortedList.forEach((worker) => console.log(`${worker.fullName}: ${worker.showSalaryWithExperience().toFixed(2)}`));
+    }
+}
+
+let worker1 = new Worker("John Johnson", 20, 23);
+console.log(worker1.fullName);
+worker1.showSalary();
+console.log("New Experience: " + worker1.showExp());
+worker1.showSalaryWithExperience();
+worker1.experience = 1.5;
+console.log("New Experience: " + worker1.showExp());
+worker1.showSalaryWithExperience();
+
+let worker2 = new Worker("Jane Johnson", 25, 22);
+worker2.experience = 1.7;
+let worker3 = new Worker("Kendall Becker", 25, 24);
+worker3.experience = 1.8;
+let worker4 = new Worker("Sally Hadson", 23, 20);
+worker4.experience = 1.9;
+let worker5 = new Worker("Emir Murphy", 20, 23);
+worker5.experience = 2.0;
+let worker6 = new Worker("Andy Carson", 27, 19);
+worker6.experience = 1.1;
+let worker7 = new Worker("Jenna Palmer", 20, 20);
+worker7.experience = 1.3;
+
+const allWorkers = [worker1, worker2, worker3, worker4, worker5, worker6, worker7];
+const listOfWorkers = new Workers(allWorkers);
+listOfWorkers.showAllWorkers();
+listOfWorkers.showSortedWorkers();
