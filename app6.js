f9aae26381c9557c3e0ed8cbd2ac244215c09fe1
ihont <ihont@softserveinc.com>
2023-11-26 14:23:44 +0200

    Added task 6.2

--- app6.js
+++ app6.js
@@ -5,3 +5,8 @@ console.log("Home Work 6");
 document.getElementById("test").innerHTML = "Last";
 document.getElementsByTagName("div")[0].innerHTML  = "Last";
 document.querySelector("#test").innerHTML  = "Last";
+
+// task 6.2
+let petImage = document.getElementsByClassName("image")[0];
+petImage.src = "cat.jpg";
+alert(petImage.outerHTML);  
