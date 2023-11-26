a122cc1cb667f784a6e389414f6266b2141fbd2e
ihont <ihont@softserveinc.com>
2023-11-26 13:48:28 +0200

    Added task 6.1

---
+++ app6.js
@@ -0,0 +1,7 @@
+// Home Work 6
+console.log("Home Work 6");
+
+// task 6.1
+document.getElementById("test").innerHTML = "Last";
+document.getElementsByTagName("div")[0].innerHTML  = "Last";
+document.querySelector("#test").innerHTML  = "Last";
