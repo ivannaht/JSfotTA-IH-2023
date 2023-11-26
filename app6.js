9fd52d734cd49b17a411f679846becaadabc6fea
ihont <ihont@softserveinc.com>
2023-11-26 17:11:16 +0200

    Added task 6.4

--- app6.js
+++ app6.js
@@ -12,5 +12,20 @@ petImage.src = "cat.jpg";
 alert(petImage.outerHTML);  
 
 // task 6.3
-let allElements = document.querySelectorAll("#text > p");
+const allElements = document.querySelectorAll("#text > p");
 allElements.forEach((element, index) => console.log(`Selector text ${index}: ${element.innerHTML}`));
+
+// task 6.4
+// way 1
+let list = document.querySelector("#list");
+let first = list.firstElementChild;
+let fifth = list.lastElementChild;
+let second = first.nextElementSibling;
+let fourth = fifth.previousElementSibling;
+let third = list.children[2];
+alert(`Output result: ${first.innerHTML}, ${fifth.innerHTML}, ${second.innerHTML}, ${fourth.innerHTML}, ${third.innerHTML}`);
+
+// way 2
+const list2 = document.querySelectorAll("#list > li");
+alert(`Output result: ${list2[0].innerHTML}, ${list2[4].innerHTML}, ${list2[1].innerHTML}, ${list2[3].innerHTML}, ${list2[2].innerHTML}`);
+
