a90ba71f354f0650306ea63c1cad05af9461f197
ivannaht <ivhontaryk@gmail.com>
2023-11-30 15:42:20 +0200

    Added task 7.1

--- app7.js
+++ app7.js
@@ -2,4 +2,11 @@
 console.log("Home Work 7");
 
 // task 7.1
-let openNew = window.open("", "", "width=300,height=300");
+let newWindow = window.open("", "", "width=300,height=300");
+
+setTimeout(() => newWindow.resizeTo(500, 500), 2000);
+
+setTimeout(() => newWindow.moveTo(200, 200), 2000);
+
+setTimeout(() => newWindow.close(), 2000);
+
