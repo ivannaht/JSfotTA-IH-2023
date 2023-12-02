eaa50dca9bae1098ee8c2d420a35af90bb1a18cd
ivannaht <ivhontaryk@gmail.com>
2023-12-02 16:35:48 +0200

    Added task 7.3

--- app7.js
+++ app7.js
@@ -4,7 +4,7 @@ console.log("Home Work 7");
 // task 7.1
 let newWindow = window.open("", "", "width=300,height=300");
 
-function windowManipulations(newWindow) {    
+function windowManipulations() {    
     setTimeout(() => {
         setTimeout (() => {
             setTimeout (() => {
@@ -19,7 +19,7 @@ function windowManipulations(newWindow) {
     }, 2000);
 }
 
-windowManipulations(newWindow);
+windowManipulations();
 
 // task 7.2
 function changeCSS() {
@@ -28,3 +28,21 @@ function changeCSS() {
     styledText.style.fontSize = "20px";
     styledText.style.fontFamily = 'Comic Sans MS';
 }
+
+
+// task 7.3
+let button1 = document.querySelector("#task-7-3-1");
+let button2 = document.querySelector("#task-7-3-2");
+let button3 = document.querySelector("#task-7-3-3");
+let link = document.querySelector("a[href='url-7-3']");
+
+function changeBackgroundColor(color) {
+    return () => {document.body.style.setProperty("background-color", color)};   
+}
+
+button1.onclick =  changeBackgroundColor("blue");
+button2.ondblclick = changeBackgroundColor("pink");
+button3.onmousedown = changeBackgroundColor("brown");
+button3.onmouseup = changeBackgroundColor("white");
+link.onmouseover = changeBackgroundColor("yellow");
+link.onmouseout = changeBackgroundColor("white");
