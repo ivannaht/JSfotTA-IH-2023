25092a3681defecf4812cea88c3aec8ed64c3277
ihont <ihont@softserveinc.com>
2023-12-06 15:41:01 +0200

    Added task 8.2

--- app8.js
+++ app8.js
@@ -7,3 +7,15 @@ const [x, y, , ...z] = arr;
 console.log(x);
 console.log(y);
 console.log(z);
+
+// task 8.2
+let data = {
+names: ["Sam", "Tom", "Ray", "Bob"],
+ages: [20, 24, 22, 26]
+};
+
+let {names: [name1, name2, name3, name4], ages: [age1, age2, age3, age4] } = data;
+console.log(name2);
+console.log(age2);
+console.log(name4);
+console.log(age4);
