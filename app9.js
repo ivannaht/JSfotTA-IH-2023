aacf87ad9f0d9829cdfb4c9ca29745b524aedc0d
ihont <ihont@softserveinc.com>
2023-12-10 13:06:24 +0200

    Added task 9.1

--- app9.js
+++ app9.js
@@ -2,3 +2,9 @@
 console.log("Home Work 9");
 
 // task 9.1
+function getPromise(message, delay) {
+    return new Promise(resolve => setTimeout(() => {resolve(message)}, delay));
+}
+
+getPromise("test promise", 2000).then((data) => console.log(data));
+    
