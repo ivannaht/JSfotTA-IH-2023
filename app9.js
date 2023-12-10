7c58ac0b223d2eb62994e93ef991f7ea609b2c9f
ihont <ihont@softserveinc.com>
2023-12-10 13:55:31 +0200

    Added task 9.2

--- app9.js
+++ app9.js
@@ -7,4 +7,21 @@ function getPromise(message, delay) {
 }
 
 getPromise("test promise", 2000).then((data) => console.log(data));
-    
+
+// task 9.2
+function calcArrProduct(arr) {
+    return new Promise((resolve) => {
+        let result = 1;
+        for (let a of arr) {  
+            if (typeof a !== "number") {
+                result = "Error! Incorrect array!";
+                break;                  
+            }
+            result *= a;            
+        }
+        resolve(result);
+    });
+}
+
+calcArrProduct([3, 4, 5]).then(result => console.log(result));
+calcArrProduct ([5, "user2", 7, 12]).then(result => console.log(result));
